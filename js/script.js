fd318bb8f8887f99b1526a42479b553e19a9f425
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-03-29 13:55:33 -0700

    Add variations for each option chosen in accordance with the requirements

--- js/script.js
+++ js/script.js
@@ -1,74 +1,47 @@
-function showInfo (location, companysize, portion, knowledge, whatfor) {
-  if (location =  "seattle") {
-    // Harry Potter characters
-    if ((orientation === 'Straight' && sex === 'Male')
-      || (orientation === 'Gay' && sex === 'Female')) {
-        if (favoriteAnimal === "Cat") {
-          match = 'Hermione Granger';
-          matchImage = 'img/HermioneGranger.jpg';
-        } else if (favoriteAnimal === "Dog") {
-          match = 'Hannah Abbott';
-          matchImage = 'img/HannahAbbott.jpg';
-        } else if (favoriteAnimal === "Snake") {
-          match = 'Bellatrix Lestrange';
-          matchImage = 'img/BellatrixLestrange.jpg';
-        } else if (favoriteAnimal === "Bird") {
-          match = 'Fleur Delacour';
-          matchImage = 'img/FleurDelacour.jpg';
+function showInfo (location, companysize, portion, knowledge) {
+  if (location = "Seattle") {
+    if ((companysize === 'Small' && portion === 'Back-End')
+      || (companysize === 'Large' && portion === 'Back-End')) {
+        if (knowledge === "New") {
+          match = 'Java';
+          matchImage = 'images/Java.png';
+        } else if (knowledge === "Some Knowledge") {
+          match = 'C#';
+          matchImage = 'images/Csharp.png';
         }
-      } else if ((orientation === 'Gay' && sex === 'Male')
-      || (orientation === 'Straight' && sex === 'Female')){
-        if (favoriteAnimal === "Cat") {
-          match = 'Harry Potter';
-          matchImage = 'img/HarryPotter.jpg';
-        } else if (favoriteAnimal === "Dog") {
-          match = 'Sirius Black';
-          matchImage = 'img/SiriusBlack.jpg';
-        } else if (favoriteAnimal === "Snake") {
-          match = 'Draco Malfoy';
-          matchImage = 'img/DracoMalfoy.jpg';
-        } else if (favoriteAnimal === "Bird") {
-          match = 'Ron Weasley';
-          matchImage = 'img/RonWeasley.jpg';
-      }
-    }
-  } else {
-    //Game of Thrones characters
-    if ((orientation === 'Straight' && sex === 'Male')
-      || (orientation === 'Gay' && sex === 'Female')) {
-        if (favoriteAnimal === "Cat") {
-          match = 'Daenerys Targaryen';
-          matchImage = 'img/DaenerysTargaryen.jpg';
-        } else if (favoriteAnimal === "Dog") {
-          match = 'Brienne of Tarth';
-          matchImage = 'img/Brienne.jpg';
-        } else if (favoriteAnimal === "Snake") {
-          match = 'Melisandre';
-          matchImage = 'img/Melisandre.jpg';
-        } else if (favoriteAnimal === "Bird") {
-          match = 'Missandei';
-          matchImage = "img/Missandei.jpg";
+      } else if ((companysize === 'Small' && portion === 'Front-End')
+        || (companysize === 'Large' && portion === 'Front-End')) {
+          if (knowledge === "New") {
+            match = 'Java';
+            matchImage = 'images/Java.png';
+          } else if (knowledge === "Some Knowledge") {
+            match = 'C#';
+            matchImage = 'images/Csharp.png';
+          }
         }
-      } else if ((orientation === 'Gay' && sex === 'Male')
-      || (orientation === 'Straight' && sex === 'Female')){
-        if (favoriteAnimal === "Cat") {
-          match = 'Khal Drogo';
-          matchImage = 'img/KhalDrogo.jpg';
-        } else if (favoriteAnimal === "Dog") {
-          match = 'John Snow';
-          matchImage = 'img/Snow.jpg';
-        } else if (favoriteAnimal === "Snake") {
-          match = 'Ramsey Bolton';
-          matchImage = 'img/Bolton.png';
-        } else if (favoriteAnimal === "Bird") {
-          match = 'Tyrion';
-          matchImage = "img/Tyrion.jpeg";
+    } else {
+    if ((companysize === 'Small' && portion === 'Front-End')
+      || (companysize === 'Large' && portion === 'Front-End')) {
+        if (knowledge === "New") {
+          match = 'Java';
+          matchImage = 'images/Java.png';
+        } else if (knowledge === "Some Knowledge") {
+          match = 'C#';
+          matchImage = 'images/Csharp.png';
+        }
+      } else if ((companysize === 'Small' && portion === 'Back-End')
+        || (companysize === 'Large' && portion === 'Back-End')) {
+          if (knowledge === "New") {
+            match = 'PHP';
+            matchImage = 'images/PHP.png';
+          } else if (knowledge === "Some Knowledge") {
+            match = 'Ruby';
+            matchImage = 'images/Ruby.png';
+          }
         }
       }
+      return [match, matchImage];
     }
-    return [match, matchImage];
-  }
-
 
 
 $(document).ready(function(){
@@ -78,9 +51,11 @@ $(document).ready(function(){
           $("#result").val((val1/val2) * .9);
     });
   $("form#question").submit(function(event) {
-    var favoriteRace = $("#favoriteRace").val();
-    var favoriteVariation = $("#favoriteVariation").val();
-    var match = showInfo(favoriteVariation, favoriteRace);
+    var location = $("#location").val();
+    var companysize = $("#companysize").val();
+    var portion = $("#portion").val();
+    var knowledge = $("#knowledge").val();
+    var match = showInfo(location, companysize, portion, knowledge);
     var matchName = match[0];
     var matchImage = match[1];
       $("#varspot").text("You would be a good fit for " + matchName + "");
